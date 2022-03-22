d2e0d94da03c2a31b55133cd3a54b57fc2bef3c9
StasiSie <mail.koshaksa@gmail.com>
2022-03-23 00:34:23 +0300

    It works!!

--- UIKitApp/ViewController.swift
+++ UIKitApp/ViewController.swift
@@ -9,10 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var canvasView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+   
+    @IBOutlet var redLevelLabel: UILabel!
+    @IBOutlet var greenLevelLabel: UILabel!
+    @IBOutlet var blueLevelLabel: UILabel!
+    
+    @IBOutlet var redLevelSlider: UISlider!
+    @IBOutlet var greenLevelSlider: UISlider!
+    @IBOutlet var blueLevelSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        canvasView.backgroundColor = UIColor (red: CGFloat(redLevelSlider.value), green: CGFloat(greenLevelSlider.value), blue: CGFloat(blueLevelSlider.value), alpha: 1)
+        
+        redLevelSlider.minimumTrackTintColor = .red
+        greenLevelSlider.minimumTrackTintColor = .green
+        blueLevelSlider.minimumTrackTintColor = .blue
+        
+        redLevelLabel.text = String(redLevelSlider.value)
+        greenLevelLabel.text = String(greenLevelSlider.value)
+        blueLevelLabel.text = String(blueLevelSlider.value)
+        
     }
+    
+    @IBAction func redSliderAction() {
+        canvasView.backgroundColor = UIColor (red: CGFloat(redLevelSlider.value), green: CGFloat(greenLevelSlider.value), blue: CGFloat(blueLevelSlider.value), alpha: 1)
+        redLevelLabel.text = String(redLevelSlider.value)
+    }
+    
+    @IBAction func greenSliderAction() {
+        canvasView.backgroundColor = UIColor (red: CGFloat(redLevelSlider.value), green: CGFloat(greenLevelSlider.value), blue: CGFloat(blueLevelSlider.value), alpha: 1)
+        greenLevelLabel.text = String(greenLevelSlider.value)
+    }
+    
+    @IBAction func blueSliderAction() {
+        canvasView.backgroundColor = UIColor (red: CGFloat(redLevelSlider.value), green: CGFloat(greenLevelSlider.value), blue: CGFloat(blueLevelSlider.value), alpha: 1)
+        blueLevelLabel.text = String(blueLevelSlider.value)
+        
+    }
+    
 
 
 }
